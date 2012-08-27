a5e1b30eb6f3cfc571a94478656f56b4fb5baba3
hjf004 <hjf004@126.com>
2012-08-27 15:08:13 +0000

    add Chinese translation

---
+++ lang.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="src/MainWindow.cpp" line="18"/>
+        <source>&amp;Open File</source>
+        <translation type="unfinished">打开(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="21"/>
+        <source>&amp;Save As...</source>
+        <translation type="unfinished">另存为(&amp;S)...</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="24"/>
+        <source>File &amp;Attribute</source>
+        <translation type="unfinished">属性(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="26"/>
+        <source>&amp;Print</source>
+        <translation type="unfinished">打印(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="29"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">退出(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="38"/>
+        <source>SlideBar</source>
+        <translation type="unfinished">侧边栏</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="43"/>
+        <source>Zoom In</source>
+        <translation type="unfinished">放大</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="46"/>
+        <source>Zoom Out</source>
+        <translation type="unfinished">缩小</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="49"/>
+        <source>&amp;Search</source>
+        <translation type="unfinished">查找(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="51"/>
+        <source>Search Next</source>
+        <translation type="unfinished">查找下一个</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="56"/>
+        <source>Select All</source>
+        <translation type="unfinished">全选</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="59"/>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">复制(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="64"/>
+        <source>&amp;ToolBar</source>
+        <translation type="unfinished">工具栏(&amp;T)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="71"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="84"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">编辑(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="91"/>
+        <source>&amp;View</source>
+        <translation type="unfinished">查看(&amp;V)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="95"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="105"/>
+        <source>ToolBar</source>
+        <translation type="unfinished">工具栏</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="156"/>
+        <source>Open a umd book</source>
+        <translation type="unfinished">打开一个UMD文件</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="156"/>
+        <source>Documents(*.umd)</source>
+        <translation type="unfinished">文档(*.umd)</translation>
+    </message>
+    <message>
+        <source>Documents(*.umd *.txt)</source>
+        <translation type="obsolete">文档(*.umd)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="200"/>
+        <source>&amp;%1 %2</source>
+        <translation type="unfinished">(&amp;%1 %2)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="292"/>
+        <source>Book Title:	%1
+Author:	%2
+Year:	%3
+Month:	%4
+Day:	%5
+Gender:	%6
+Publisher:	%7
+Vendor:	%8</source>
+        <translation type="unfinished">书名:\t%1\n作者:\t%2\n年份:\t%3\n月份:\t%4\n日期:\t%5\n分类:\t%6\n出版商:\t%7\n 零售商:\t%8 </translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="296"/>
+        <source>File Attribute</source>
+        <translation type="unfinished">属性</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="305"/>
+        <source>Save File As...</source>
+        <translation type="unfinished">另存为...</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="306"/>
+        <source>Documents (*.txt)</source>
+        <translation type="unfinished">文本文件(*.txt)</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="337"/>
+        <source>Search A String</source>
+        <translation type="unfinished">查找字符串</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="338"/>
+        <source>Input the string to search:</source>
+        <translation type="unfinished">请输入查找的字符串:</translation>
+    </message>
+    <message>
+        <location filename="src/UmdReader.cpp" line="27"/>
+        <source>Chapter List</source>
+        <translation type="unfinished">章节列表</translation>
+    </message>
+</context>
+</TS>
